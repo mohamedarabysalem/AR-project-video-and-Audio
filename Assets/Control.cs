de3702eb5f83e11c628e38a175cbbd921b3d8a3a
Mohamed El-araby Salem <mohamed16araby@gmail.com>
2017-10-05 17:17:15 -0700

    Add files via upload

---
+++ Assets/Control.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+	// Use this for initialization
+	public float rotationSpeed=70.0f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.FindWithTag("house").transform.Rotate(0, 0, rotationSpeed * Time.deltaTime );
+		GameObject.FindWithTag("house1").transform.Rotate(0, 0, rotationSpeed * Time.deltaTime );
+		GameObject.FindWithTag("house2").transform.Rotate(0, 0, rotationSpeed * Time.deltaTime );
+
+
+		
+	}
+}
